45624c49bdedf09580ee8b3b4b1750f8b26274d7
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2022-04-11 08:15:06 +0300

    Register HttpClientModule.

--- my-store/src/app/cart/cart.component.ts
+++ my-store/src/app/cart/cart.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
+
 export class CartComponent {
 
-  constructor() { }
+  items = this.cartService.getItems();
+  
+  constructor(private cartService: CartService) { }
 }
